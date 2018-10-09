367727851a216e736a30d909c303da86589d8f64
ki1556ki <ki1556ki@naver.com>
2018-10-09 20:00:40 +0900

    Android
    사진촬영후 사진 가져오기 구현
    앨범에서 사진 가져오기 구현

--- AndroidStudioProjects/TeamProject1/app/src/main/java/com/apress/gerber/teamproject1/CameraActivity.java
+++ AndroidStudioProjects/TeamProject1/app/src/main/java/com/apress/gerber/teamproject1/CameraActivity.java
@@ -1,75 +1,55 @@
 package com.apress.gerber.teamproject1;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import java.io.File;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.FileProvider;
 import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import android.widget.ImageView;
-import android.widget.Toast;
 
 
-import java.io.File;
-import java.io.IOException;
+public class CameraActivity extends Activity implements View.OnClickListener{
+    static String SAMPLEIMG=".test.jpg";
+    static File file = new File(Environment.getExternalStorageDirectory(), SAMPLEIMG);
+    static final int REQUEST_PICTURE=1;
 
-/**
- * 인텐트를 이용해 단말의 카메라 앱을 실행시켜 사진을 찍는 방법을 알 수 있습니다.
- *
- */
-public class CameraActivity extends AppCompatActivity {
-    public static final int REQUEST_IMAGE_CAPTURE = 1001;
-
-    File file = null;
-    ImageView imageView;
+    ImageView iv;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-
-        imageView = (ImageView) findViewById(R.id.imageView);
-
-        try {
-            file = createFile();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-    }
-
-    public void onButton1Clicked(View v) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    private File createFile() throws IOException {
-        String imageFileName = "test.jpg";
-        File storageDir = Environment.getExternalStorageDirectory();
-        File curFile = new File(storageDir, imageFileName);
-
-        return curFile;
+        iv=(ImageView) findViewById(R.id.imgView);
+        findViewById(R.id.getCustom).setOnClickListener(this);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 8;
-            if (file != null) {
-                Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-
-                imageView.setImageBitmap(bitmap);
-            } else {
-                Toast.makeText(getApplicationContext(), "File is null.", Toast.LENGTH_LONG).show();
-            }
+    public void onClick(View v){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(this, "com.apress.gerber.teamproject1.fileprovider", file));
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, REQUEST_PICTURE);
         }
     }
-
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 8;
+        Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+        iv.setImageBitmap(bitmap);
+    }
 }
+
+
+
+
+
+
+
--- AndroidStudioProjects/TeamProject1/app/src/main/java/com/apress/gerber/teamproject1/MainActivity.java
+++ AndroidStudioProjects/TeamProject1/app/src/main/java/com/apress/gerber/teamproject1/MainActivity.java
@@ -1,26 +1,19 @@
 package com.apress.gerber.teamproject1;
 
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import android.provider.MediaStore;
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import android.view.textclassifier.TextLinks;
-import android.widget.Toast;
-
-
 
 public class MainActivity extends AppCompatActivity{
 
-    public static final int REQUEST_CODE_MENU = 101;
+    static final int CAMERA_ACTIVITY_CODE=1;
+    static final int ALBUM_ACTIVITY_CODE=2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +21,10 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-//        if (permissionCheck == PackageManager.PERMISSION_GRANTED){
-//            Toast.makeText(this, "카메라 권한 있음.", Toast.LENGTH_LONG).show();
-//        }else {
-//            Toast.makeText(this, "카메라 권한 없음", Toast.LENGTH_LONG).show();
-//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
-//                Toast.makeText(this, "카메라 권한 설명 필요함.", Toast.LENGTH_LONG).show();
-//            }else {
-//                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1);
-//            }
-//        }
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -56,8 +35,6 @@ public class MainActivity extends AppCompatActivity{
             case R.id.settings:
                 return true;
             case R.id.exit:
-//                View view = (View) findViewById(R.id.exit);
-//                ExitClick(view);
                 finish();
                 return true;
             default:
@@ -66,18 +43,13 @@ public class MainActivity extends AppCompatActivity{
     }
 
     public void CameraClick(View v) {
-//        Intent iCamera =  new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
-//        Toast.makeText(getApplicationContext(), "사진을 찍어주세요.", Toast.LENGTH_LONG).show();
-//        startActivityForResult(iCamera, 0);
-        Intent intent = new Intent(getApplicationContext(), CameraActivity.class) ;
-        startActivityForResult(intent, REQUEST_CODE_MENU);
+       Intent iCamera = new Intent(this, CameraActivity.class);
+       startActivityForResult(iCamera, CAMERA_ACTIVITY_CODE);
     }
 
     public void FolderClick(View v){
-        Intent iFolder = new Intent(Intent.ACTION_PICK);
-        iFolder.setType(MediaStore.Images.Media.CONTENT_TYPE);
-        Toast.makeText(getApplicationContext(), "사진을 선택해 주세요", Toast.LENGTH_LONG).show();
-        startActivityForResult(iFolder, 0);
+        Intent iFolder = new Intent(this, AlbumActivity.class);
+        startActivityForResult(iFolder, ALBUM_ACTIVITY_CODE);
     }
 
     public void Button1Click(View v){
@@ -89,53 +61,4 @@ public class MainActivity extends AppCompatActivity{
         Intent iButton2  = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
         startActivity(iButton2);
     }
-
-//    public void ExitClick(View v){
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//        builder.setTitle("안내");
-//        builder.setMessage("어딜 도망가");
-//        builder.setIcon(android.R.drawable.ic_dialog_alert);
-//        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                String message = "예 버튼이 눌렸습니다.";
-//                textView.setText(message);
-//            }
-//        });
-//
-//        builder.setNeutralButton("취소", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                String message = "취소 버튼이 눌렸습니다.";
-//                textView.setText(message);
-//            }
-//        });
-//
-//        builder.setNeutralButton("아니오", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                String message = "아니오 버튼이 눌렸습니다.";
-//                textView.setText(message);
-//            }
-//        });
-//
-//        AlertDialog dialog = builder.create();
-//        dialog.show();
-//
-//    }
-
-
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        switch (requestCode){
-//            case 1:{
-//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-//                    Toast.makeText(this, "카메라 권한을 사용자가 승인함.", Toast.LENGTH_LONG).show();
-//                }else {
-//                    Toast.makeText(this, "카메라 권한 거부됨.", Toast.LENGTH_LONG).show();
-//                }
-//                return;
-//            }
-//        }
-//    }
 }
