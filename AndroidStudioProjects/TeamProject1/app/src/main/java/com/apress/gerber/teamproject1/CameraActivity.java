d7eb67516919720a122f86cc82af2c277e8db821
ki1556ki <ki1556ki@naver.com>
2018-10-28 18:18:54 +0900

    Revert "Android"
    This reverts commit 367727851a216e736a30d909c303da86589d8f64.

--- AndroidStudioProjects/TeamProject1/app/src/main/java/com/apress/gerber/teamproject1/CameraActivity.java
+++ AndroidStudioProjects/TeamProject1/app/src/main/java/com/apress/gerber/teamproject1/CameraActivity.java
@@ -1,57 +1,93 @@
 package com.apress.gerber.teamproject1;
 
+<<<<<<< HEAD
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+=======
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+>>>>>>> parent of 3677278... Android
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 
 import java.io.File;
+<<<<<<< HEAD
 
+=======
+import java.io.IOException;
+>>>>>>> parent of 3677278... Android
 
-public class CameraActivity extends Activity implements View.OnClickListener{
-    static String SAMPLEIMG=".test.jpg";
-    static File file = new File(Environment.getExternalStorageDirectory(), SAMPLEIMG);
-    static final int REQUEST_PICTURE=1;
+/**
+ * 인텐트를 이용해 단말의 카메라 앱을 실행시켜 사진을 찍는 방법을 알 수 있습니다.
+ *
+ */
+public class CameraActivity extends AppCompatActivity {
+    public static final int REQUEST_IMAGE_CAPTURE = 1001;
 
-    ImageView iv;
+    File file = null;
+    ImageView imageView;
 
     @Override
+<<<<<<< HEAD
     protected void onCreate(Bundle savedInstanceState){
 
 
 
+=======
+    protected void onCreate(Bundle savedInstanceState) {
+>>>>>>> parent of 3677278... Android
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        iv=(ImageView) findViewById(R.id.imgView);
-        findViewById(R.id.getCustom).setOnClickListener(this);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        try {
+            file = createFile();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void onButton1Clicked(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private File createFile() throws IOException {
+        String imageFileName = "test.jpg";
+        File storageDir = Environment.getExternalStorageDirectory();
+        File curFile = new File(storageDir, imageFileName);
+
+        return curFile;
     }
 
     @Override
-    public void onClick(View v){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(this, "com.apress.gerber.teamproject1.fileprovider", file));
-        if (intent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(intent, REQUEST_PICTURE);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;
+            if (file != null) {
+                Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+
+                imageView.setImageBitmap(bitmap);
+            } else {
+                Toast.makeText(getApplicationContext(), "File is null.", Toast.LENGTH_LONG).show();
+            }
         }
     }
-    protected void onActivityResult(int requestCode,int resultCode,Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 8;
-        Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-        iv.setImageBitmap(bitmap);
-    }
+
 }
-
-
-
-
-
-
-
